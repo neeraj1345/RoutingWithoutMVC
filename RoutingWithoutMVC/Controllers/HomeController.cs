5baaed9eb5aa1c2111176a5df5963bc0d2176dfe
shubham <shubham.choudhary@bellurbis.com>
2023-09-08 12:05:10 +0530

    Add project files.

---
+++ RoutingWithoutMVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingWithoutMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Home/About")]
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("Home/Details/{id?}")]
+
+        public int Details(int id)
+        {
+            return id;
+        }
+
+    }
+
+
+
+}
---
+++ RoutingWithoutMVC/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews(); 
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+//app.MapDefaultControllerRoute();
+
+//app.MapControllerRoute(
+
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}"
+
+//    );
+
+app.MapControllers();
+
+
+app.Run();
